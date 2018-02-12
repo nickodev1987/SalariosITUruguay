8185bce9ef2eb4eeb00784d15ba48b5dbf89f36f
Marlon Gonzalez Gongora <nickoo1987@gmail.com>
2018-02-11 22:51:53 -0300

    Added generic repository and database project

---
+++ SalariosITUruguayDB/dbo/Tables/Position.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Position] (
+    [ID]       INT            IDENTITY (1, 1) NOT NULL,
+    [Position] NVARCHAR (250) NOT NULL,
+    CONSTRAINT [PK_Position] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
---
+++ SalariosITUruguayDB/dbo/Tables/Respondent.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Respondent] (
+    [ID]       INT            IDENTITY (1, 1) NOT NULL,
+    [User]     NVARCHAR (50)  NOT NULL,
+    [Password] NVARCHAR (250) NOT NULL,
+    [Email]    NVARCHAR (250) NULL,
+    [WorksFor] INT            NOT NULL,
+    [Role]     INT            NOT NULL,
+    [Salary]   MONEY          NOT NULL,
+    CONSTRAINT [PK_Encuestado] PRIMARY KEY CLUSTERED ([ID] ASC),
+    CONSTRAINT [FK_Respondent_Company] FOREIGN KEY ([WorksFor]) REFERENCES [dbo].[Company] ([ID]),
+    CONSTRAINT [FK_Respondent_Position] FOREIGN KEY ([Role]) REFERENCES [dbo].[Position] ([ID])
+);
+
